93083695c130ef5fce37dcc83fbc5a9ff56b56ac
Ashok kumar Ganji <123137729+AshokkumarGanji@users.noreply.github.com>
2023-09-19 13:52:48 -0500

    Calculator App Initial commit
    Work in progress

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Ganji,Ashok Kumar on 9/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    //All the outlet  connection variables
+    
+    
+    @IBOutlet weak var resultOutlet: UILabel!
+    
+    @IBOutlet weak var AcOutlet: UIButton!
+    
+    @IBOutlet weak var cbuttonOutlect: UIButton!
+    
+    @IBOutlet weak var plusminusbuttonOutlet: UIButton!
+    
+    @IBOutlet weak var divbuttonOutlet: UIButton!
+    
+    @IBOutlet weak var buttonSeven: UIButton!
+    
+    @IBOutlet weak var buttonEight: UIButton!
+    
+    @IBOutlet weak var buttonNine: UIButton!
+    
+    @IBOutlet weak var buttonMul: UIButton!
+    
+    @IBOutlet weak var buttonFour: UIButton!
+    
+    @IBOutlet weak var buttonFive: UIButton!
+    
+    @IBOutlet weak var buttonSix: UIButton!
+    
+    @IBOutlet weak var buttonMinus: UIButton!
+    
+    @IBOutlet weak var buttonOne: UIButton!
+    
+    @IBOutlet weak var buttonTwo: UIButton!
+    
+    @IBOutlet weak var buttonThree: UIButton!
+    
+    @IBOutlet weak var buttonPlus: UIButton!
+    
+    @IBOutlet weak var buttonZero: UIButton!
+    
+    @IBOutlet weak var buttonDot: UIButton!
+    
+    @IBOutlet weak var buttonPercentage: UIButton!
+    
+    @IBOutlet weak var buttonEquals: UIButton!
+    
+    
+    
+    // The Actions fucntions for all the respective buttons.
+    @IBAction func AcButton(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func CButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func PlusMinusButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func DivButton(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonSeven(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonEight(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonNine(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonMul(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ButtonFour(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonFive(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonSix(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonMinus(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonOne(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ButtonTwo(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ButtonThree(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonPlus(_ sender: Any) {
+    }
+    
+    
+    @IBAction func BUttonZero(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonDot(_ sender: Any) {
+    }
+    
+    @IBAction func ButtonPercentage(_ sender: Any) {
+    }
+    
+    
+    @IBAction func ButtonEquals(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
