f34f63f4abe4552461532f7483ec6f7acf053c4d
Ashok kumar Ganji <123137729+AshokkumarGanji@users.noreply.github.com>
2023-11-09 13:49:43 -0600

    Student App

---
+++ StudentApp/StudentApp/LoginViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  StudentApp
+//
+//  Created by Ashok on 11/7/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var sIdOutlet: UITextField!
+    
+    //creating a global variable for holding a student
+    var studentFound = Student()
+    
+    //to check whether user is student/guest
+        //Initially isStudent is false that means user is a guest
+        var isStudent = false
+    
+    
+    //Array of type Student, we imported it from the 'Student' file
+        var studentsArray = students
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+        
+        
+        let enteredID = sIdOutlet.text!
+        
+        //Loop the array to find the student
+        for Student in studentsArray{
+            
+            if enteredID == Student.sid{
+                
+                //student found and store the student in a global variable.
+                studentFound = Student
+                
+                //boolean flag as true,since we found a student.
+                isStudent = true
+                
+            }
+        }
+    } // End of LoginButton
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let transition = segue.identifier
+        
+        if transition == "studentInfoSegue"{
+            
+            
+            //Create a destination of type studentInfoViewController
+            let destination = segue.destination as! StudentInfoViewController
+            
+            //if student is exists in the array, we will assign the studentObj in the destination with "studentFound"
+
+            
+            if isStudent{
+                
+                destination.studentObj = studentFound
+            }else{
+                
+                //if the given sid is not in the array, then the student is a guest!!
+                                //we set the boolean in the destination as true!!
+                destination.guestUser = true
+                
+            }
+        }
+        
+        
+    }
+    
+    
+    
+
+}// End of Main
+
